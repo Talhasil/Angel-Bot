f0001a0a30e7274d3a062ff5acb8066a059ba27a
Talhasil <96937411+Talhasil@users.noreply.github.com>
2022-04-17 16:55:58 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name:"yardım",
+  aliases:"help",
+  code:`
+  $author[1;Flora Bot Komutları;$userAvatar[$clientID]]
+  $addField[1;Diğer;\`\`\`js
+eval|vip\`\`\`]
+  $addField[1;Yetkili;\`\`\`js
+ban|unban|mute|unmute|abone|sil|lock|unlock\`\`\`]
+  $addField[1;Kullanıcı;\`\`\`js
+avatar|banner|afk|tavsiye|puan|function|yt|profil|say|sunucu\`\`\`]
+  $thumbnail[1;$serverIcon]
+  $color[1;RANDOM]
+  `
+  }
