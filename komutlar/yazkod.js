f32c087e3a75bbcb1ccdaa8250d71ceb2b1c5d39
Talhasil <96937411+Talhasil@users.noreply.github.com>
2022-07-25 15:29:59 +0300

    Delete yazkod.js

--- komutlar/yazkod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name:"yazkodyetkili",
-  code:`
- $customEmoji[hypesquad] **Bu Kod** \`Flora Development'a\` **Aittir,Çalınması Kesinlikle Yasaktır Çalındığı Görüldüğü Takdirde Yöneticilere Bildirilmelidir** $customEmoji[hypesquad]
-  $deleteCommand
-  $onlyForRoles[940931380106379295;Yetkin Yok]
-  `
-}
